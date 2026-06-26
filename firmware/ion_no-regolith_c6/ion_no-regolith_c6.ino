92f3fb6bec782233dc002e3da06cefc1acebfd83
ualiberik <uali.berik.2010@gmail.com>
2026-06-26 13:50:19 +0500

    Add ion_no-regolith_c6

--- firmware/ion_no-regolith_c6/ion_no-regolith_c6.ino
+++ firmware/ion_no-regolith_c6/ion_no-regolith_c6.ino
@@ -1,3 +1,7 @@
+// This firmware is for no-regolith configuration of payload cube for ESP32-C6
+// It imitates deep sleep cycle of ion_2026 and ion_2026_berikov
+// It collects and writes data the same way as ion_2026 and ion_2026_berikov
+
 void setup() {
   // put your setup code here, to run once:
 
